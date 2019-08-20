978407f079f1fccae60e3fd8cb93f16548f288ab
Andrei <n3rd4i@gmail.com>
2019-08-20 20:16:09 +0200

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/OpenRA/OpenRA/releases/download/release-20190314/OpenRA-release-20190314.exe'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'openra*'
+  checksum      = 'E0D51ABD55098E4125B826AD03C092DAD620E4A1674A7B75CCC53708A3BEE73F'
+  checksumType  = 'sha256'
+  silentArgs    = "/S /Q"
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs
